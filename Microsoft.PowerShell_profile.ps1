5963a4af9e6ebd55ac4f012396e574d4b0d8c347
mi-skam <maksim.codes@mailbox.org>
2020-08-09 10:15:11 +0200

    small clean-ups in $profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,4 @@
-#Import-Module posh-git
 Import-Module PSReadline
-
-# Load my scripts (not tests) automatically
-Resolve-Path $PSScriptRoot\Scripts\*.ps1 |
-Where-Object { -not ($_.ProviderPath.Contains(".Tests.")) } |
-Foreach-Object { . $_.ProviderPath }
 
 # user Emacs key binding
 Set-PSReadLineOption -EditMode Emacs
@@ -14,3 +8,8 @@ Set-PSReadlineOption -BellStyle None
 
 function which($cmd) { (Get-Command $cmd).Definition }
 function whoami { (get-content env:\userdomain) + "\" + (get-content env:\username) }
+
+# Load my scripts (not tests) automatically
+Resolve-Path $PSScriptRoot\Scripts\*.ps1 |
+Where-Object { -not ($_.ProviderPath.Contains(".Tests.")) } |
+Foreach-Object { . $_.ProviderPath }
